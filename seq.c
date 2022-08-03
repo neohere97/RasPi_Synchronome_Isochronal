676204cd30755264d1f94ac69295b54292d286c8
neohere97 <shalawadi.chinmay@gmail.com>
2022-08-02 19:27:12 -0600

    t

--- seq.c
+++ seq.c
@@ -17,12 +17,6 @@ typedef struct
     int threadIdx;
 } threadParams_t;
 
-struct timespecd {
-    time_t tv_sec;
-    long tv_nsec;
-} sleep_time;
-
-
 // POSIX thread declarations and scheduling attributes
 //
 pthread_t threads[NUM_THREADS];
@@ -38,110 +32,113 @@ struct sched_param fifo_param;
 
 double getTimeMsec(void)
 {
-  struct timespec event_ts = {0, 0};
+    struct timespec event_ts = {0, 0};
 
-  clock_gettime(CLOCK_REALTIME, &event_ts);
-  return ((event_ts.tv_sec)*1000.0) + ((event_ts.tv_nsec)/1000000.0);
+    clock_gettime(CLOCK_REALTIME, &event_ts);
+    return ((event_ts.tv_sec) * 1000.0) + ((event_ts.tv_nsec) / 1000000.0);
 }
 
 void *Sequencer(void *threadp)
 {
-  sleep_time.tv_sec = 1;
-  sleep_time.tv_nsec = 0;
+    struct timespec
+    {
+        time_t tv_sec;
+        long tv_nsec;
+    } sleep_time;
 
-  while(1){   
-      printf("This should get printed ever 1 second \n");
-      nanosleep(sleep_time);
+    sleep_time.tv_sec = 1;
+    sleep_time.tv_nsec = 0;
+
+    while (1)
+    {
+        printf("This should get printed ever 1 second \n");
+        nanosleep(&sleep_time);
     }
-
 }
 
 void print_scheduler(void)
 {
     int schedType = sched_getscheduler(getpid());
 
-    switch(schedType)
+    switch (schedType)
     {
-        case SCHED_FIFO:
-            printf("Pthread policy is SCHED_FIFO\n");
-            break;
-        case SCHED_OTHER:
-            printf("Pthread policy is SCHED_OTHER\n");
-            break;
-        case SCHED_RR:
-            printf("Pthread policy is SCHED_RR\n");
-            break;
-        default:
-            printf("Pthread policy is UNKNOWN\n");
+    case SCHED_FIFO:
+        printf("Pthread policy is SCHED_FIFO\n");
+        break;
+    case SCHED_OTHER:
+        printf("Pthread policy is SCHED_OTHER\n");
+        break;
+    case SCHED_RR:
+        printf("Pthread policy is SCHED_RR\n");
+        break;
+    default:
+        printf("Pthread policy is UNKNOWN\n");
     }
 }
-
 
 void set_scheduler(int cpu_id)
 {
     int max_prio, scope, rc, cpuidx;
     cpu_set_t cpuset;
 
-    printf("INITIAL "); print_scheduler();
+    printf("INITIAL ");
+    print_scheduler();
 
     pthread_attr_init(&fifo_sched_attr);
     pthread_attr_setinheritsched(&fifo_sched_attr, PTHREAD_EXPLICIT_SCHED);
     pthread_attr_setschedpolicy(&fifo_sched_attr, SCHED_POLICY);
     CPU_ZERO(&cpuset);
-    cpuidx=(cpu_id);
+    cpuidx = (cpu_id);
     CPU_SET(cpuidx, &cpuset);
     pthread_attr_setaffinity_np(&fifo_sched_attr, sizeof(cpu_set_t), &cpuset);
 
-    max_prio=sched_get_priority_max(SCHED_POLICY);
-    fifo_param.sched_priority=max_prio;    
+    max_prio = sched_get_priority_max(SCHED_POLICY);
+    fifo_param.sched_priority = max_prio;
 
-    if((rc=sched_setscheduler(getpid(), SCHED_POLICY, &fifo_param)) < 0)
+    if ((rc = sched_setscheduler(getpid(), SCHED_POLICY, &fifo_param)) < 0)
         perror("sched_setscheduler");
 
     pthread_attr_setschedparam(&fifo_sched_attr, &fifo_param);
 
-    printf("ADJUSTED "); print_scheduler();
+    printf("ADJUSTED ");
+    print_scheduler();
 }
 
-
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-   int rc;
-   int i, j;
-   cpu_set_t cpuset;
+    int rc;
+    int i, j;
+    cpu_set_t cpuset;
 
-   set_scheduler(2);
+    set_scheduler(2);
 
-   CPU_ZERO(&cpuset);
+    CPU_ZERO(&cpuset);
 
-   // get affinity set for main thread
-   mainthread = pthread_self();
+    // get affinity set for main thread
+    mainthread = pthread_self();
 
-   // Check the affinity mask assigned to the thread 
-   rc = pthread_getaffinity_np(mainthread, sizeof(cpu_set_t), &cpuset);
-   if (rc != 0)
-       perror("pthread_getaffinity_np");
-   else
-   {
-       printf("main thread running on CPU=%d, CPUs =", sched_getcpu());
+    // Check the affinity mask assigned to the thread
+    rc = pthread_getaffinity_np(mainthread, sizeof(cpu_set_t), &cpuset);
+    if (rc != 0)
+        perror("pthread_getaffinity_np");
+    else
+    {
+        printf("main thread running on CPU=%d, CPUs =", sched_getcpu());
 
-       for (j = 0; j < CPU_SETSIZE; j++)
-           if (CPU_ISSET(j, &cpuset))
-               printf(" %d", j);
+        for (j = 0; j < CPU_SETSIZE; j++)
+            if (CPU_ISSET(j, &cpuset))
+                printf(" %d", j);
 
-       printf("\n");
-   }
+        printf("\n");
+    }
 
-   pthread_create(&startthread,   // pointer to thread descriptor
-                  &fifo_sched_attr,     // use FIFO RT max priority attributes
-                  Sequencer, // thread function entry point
-                  (void *)0 // parameters to pass in
-                 );
+    pthread_create(&startthread,     // pointer to thread descriptor
+                   &fifo_sched_attr, // use FIFO RT max priority attributes
+                   Sequencer,        // thread function entry point
+                   (void *)0         // parameters to pass in
+    );
 
-   pthread_join(startthread, NULL);
+    pthread_join(startthread, NULL);
 
-   printf("\nTEST COMPLETE\n");
+    printf("\nTEST COMPLETE\n");
 }
-
-
