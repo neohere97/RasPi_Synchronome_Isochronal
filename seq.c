15048d2bf526399b47decb81e5458cc8b799c298
neohere97 <shalawadi.chinmay@gmail.com>
2022-08-06 00:57:18 -0600

    maybe

--- seq.c
+++ seq.c
@@ -301,8 +301,8 @@ static void dump_ppm(const void *p, int size, unsigned int tag, struct timespec 
     close(dumpfd);
 }
 
-char pgm_header = "P5\n#9999999999 sec 9999999999 msec \n" HRES_STR " " VRES_STR "\n255\n";
-char pgm_dumpname = "frames/test0000.pgm";
+char pgm_header[] = "P5\n#9999999999 sec 9999999999 msec \n" HRES_STR " " VRES_STR "\n255\n";
+char pgm_dumpname[] = "frames/test0000.pgm";
 char *uname_header;
 char *new_header;
 static void dump_pgm(const void *p, int size, unsigned int tag, struct timespec *time)
@@ -320,8 +320,8 @@ static void dump_pgm(const void *p, int size, unsigned int tag, struct timespec 
     strncat(&pgm_header[29], " msec \n" HRES_STR " " VRES_STR "\n255\n", 19);
     snprintf(&pgm_header[19], 11, "%010d", (int)((time->tv_nsec) / 1000000));
     sprintf(&uname_header,"%S %S %S %S \n", sysname.sysname,sysname.nodename,sysname.release, sysname.machine);
-    strcat(&new_header, &pgm_header);
-    strcat(&new_header, &uname_header);
+    strcat(new_header, pgm_header);
+    strcat(new_header, uname_header);
 
     written = write(dumpfd, pgm_header, sizeof(pgm_header));
 
