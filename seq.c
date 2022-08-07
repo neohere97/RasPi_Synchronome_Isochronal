32b0533b273a8fd5a089a08bdbda188fdd28e06e
neohere97 <shalawadi.chinmay@gmail.com>
2022-08-07 14:21:58 -0600

    tes

--- seq.c
+++ seq.c
@@ -1180,7 +1180,7 @@ void *frame_selector(void *threadparams)
                 {
                     diff = abs(temp_buffer[i] - acqbuffer[acq_buf_pending].frame_data[i]);
                     
-                    if(diff > 20)
+                    if(diff > 30)
                     frame_diff_avg += diff;
                 
                 }
